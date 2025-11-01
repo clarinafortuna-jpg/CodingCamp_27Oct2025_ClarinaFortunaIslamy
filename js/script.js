136ff1caa01daae170f6d580e938dd5219bca8e4
unknown <clarinafortuna@gmail.com>
2025-11-01 09:01:22 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
-console.log('Hallo');
+
+/// Local array to store todo items
+let todos = [];
+
+function validateform(todo, date) {
+    if (todo.trim() === '' || date === '') {
+        return false;
+    }
+    return true;
+}
+
+
+function addTodo() {
+    const todoInput = document.getElementById( 'todo-input' ).value;
+    const todoDate = document.getElementById( 'todo-date' ).value;
+
+    if (!validateform(todoInput, todoDate)) {   
+        alert('Form validation invalid. Please check your inputs.');
+    } else {
+        //Add todo to local array
+        todos.push({ task: todoInput, dueDate: todoDate }) ;
+
+        renderTodos();
+    }
+} 
+
+function deleteTodo() {
+
+}
+
+function filterTodo() {
+    
+}
+
+function renderTodos() {
+    const todoList = document.getElementById('todo-list');
+
+    // Clear existing list
+    todoList.innerHTML += '';
+
+    // Render each todo item
+    todos.forEach((todo, index) => {
+        todoList.innerHTML += `<li>
+            <span>${todo.task} - ${todo.dueDate}</span>
+        </li>`;
+    });
+} 
